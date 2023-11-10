12171c5a13c8edc67a76a584199ed0127fcfff92
unknown <alegagonz@gmail.com>
2023-11-10 13:33:42 -0300

    gitignore file

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -1,6 +1,4 @@
 const { expect } = require('@wdio/globals')
-// const LoginPage = require('../pageobjects/login.page')
-// const SecurePage = require('../pageobjects/secure.page')
 const HomePage = require('../pageobjects/home.page')
 const ProductPage = require('../pageobjects/product.page')
 const CartPage = require('../pageobjects/cart.page')
@@ -18,27 +16,16 @@ describe('demo app', () => {
         await HomePage.open("")
 
         await HomePage.loginUser("pruebaalejandro20231109-2", "password")
-        // // await LoginPage.open()
-        // await HomePage.selectLaptop()
-        // await browser.pause(5000)
-        // await ProductPage.addToCart()
-        // await HomePage.logoutUser()
     })
     it('should add laptop to cart', async () => {
        await HomePage.selectLaptop()
         await browser.pause(5000)
         await ProductPage.addToCart()
-
-
-        // await HomePage.logoutUser()
     })
 
     it('cart should contain items', async () => {
-
- 
          await CartPage.open("cart.html")
          await CartPage.validateCartNotEmpty()
-         // await HomePage.logoutUser()
      })
 
 
