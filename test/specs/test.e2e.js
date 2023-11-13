57d07fc30c784ffe034d61260fb2fe42f6090b1f
alejandro gonzalez <alegagonz@gmail.com>
2023-11-13 15:03:23 -0300

    savescreenshot

--- test/specs/test.e2e.js
+++ test/specs/test.e2e.js
@@ -8,18 +8,19 @@ describe('blaze demo application', () => {
     it('should sign up user', async () => {
         await HomePage.open("")
 
-        await HomePage.signupUser("pruebaalejandro20231109-2", "password")
+        await HomePage.signupUser("pruebaalejandro20231109-3", "password")
         
       
     })
     it('should log in', async () => {
         await HomePage.open("")
 
-        await HomePage.loginUser("pruebaalejandro20231109-2", "password")
+        await HomePage.loginUser("pruebaalejandro20231109-3", "password")
     })
     it('should add laptop to cart', async () => {
        await HomePage.selectLaptop()
         await browser.pause(5000)
+        await browser.saveScreenshot('./screenshot.png')
         await ProductPage.addToCart()
     })
 
